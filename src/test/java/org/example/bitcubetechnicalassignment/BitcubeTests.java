aad131a9804289385415ffbaed697b4a55530d48
chrismkhontwana <33213375+chrismkhontwana@users.noreply.github.com>
2026-09-03 14:04:21 +0200

    Created base and added a test
    - Create base class
    - Added validateStylingForEmptyInput test case

--- src/test/java/org/example/bitcubetechnicalassignment/BitcubeTests.java
+++ src/test/java/org/example/bitcubetechnicalassignment/BitcubeTests.java
@@ -1,47 +1,21 @@
 package org.example.bitcubetechnicalassignment;
 
 import com.microsoft.playwright.*;
-
 import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-public class BitcubeTests
+public class BitcubeTests extends BitcubeBase
 {
-    // Shared between all tests in this class.
-    static Playwright playwright;
-    static Browser browser;
-
-    // New instance for each test method.
-    BrowserContext context;
-    Page page;
-
-
-    @BeforeAll
-    static void launchBrowser() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
-    }
-    @BeforeEach
-    void createPage() {
-        context = browser.newContext();
-        page = context.newPage();
-
-        page.navigate("http://qainterview.pythonanywhere.com");
-    }
-    @AfterAll
-    static void tearDown()
+    @Test
+    void validateStylingForEmptyInput()
     {
-        browser.close();
-        playwright.close();
-    }
+        String expectedStyle = "border: 2px solid red";
+        Locator calculateButton = page.locator("#getFactorial");
+        calculateButton.click();
+        String style = page.locator("#number").getAttribute("style");
 
-    @AfterEach
-    void closeContext()
-    {
-        context.close();
-    }
+        System.out.println("Style: " + style);
+        assertTrue(style.contains(expectedStyle), "Expected red border, but actual style was: " + style);
 
+    }
 }
