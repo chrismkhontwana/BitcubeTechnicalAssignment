c9ab7e4122a232f8df09f9d68f1ada54e20b743a
chrismkhontwana <33213375+chrismkhontwana@users.noreply.github.com>
2026-09-03 15:01:41 +0200

    Added the second test
    added calculateFactorialOf12

--- src/test/java/org/example/bitcubetechnicalassignment/BitcubeTests.java
+++ src/test/java/org/example/bitcubetechnicalassignment/BitcubeTests.java
@@ -2,6 +2,9 @@ package org.example.bitcubetechnicalassignment;
 
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BitcubeTests extends BitcubeBase
@@ -18,4 +21,25 @@ public class BitcubeTests extends BitcubeBase
         assertTrue(style.contains(expectedStyle), "Expected red border, but actual style was: " + style);
 
     }
+
+    @Test
+    void calculateFactorialOf12()
+    {
+        String expectedNumber = "479001600";
+        String factorOfNumber = "12";
+        Locator numberInput = page.locator("#number");
+        numberInput.fill(factorOfNumber);
+
+        Locator calculateButton = page.locator("#getFactorial");
+        calculateButton.click();
+
+        Locator result = page.locator("#resultDiv");
+        assertThat(result).containsText("The factorial of ");
+
+        String text = result.textContent().trim();
+        String number = text.substring(text.lastIndexOf(":") + 1).trim();
+
+        assertEquals(expectedNumber, number);
+    }
+
 }
