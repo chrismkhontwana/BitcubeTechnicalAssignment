da5160f4db38c994b813cd901b17820e760de58b
chrismkhontwana <33213375+chrismkhontwana@users.noreply.github.com>
2026-09-03 15:19:47 +0200

    Updated BitcubeTests
    Added validateFactorialApiRequest test

--- src/test/java/org/example/bitcubetechnicalassignment/BitcubeTests.java
+++ src/test/java/org/example/bitcubetechnicalassignment/BitcubeTests.java
@@ -3,9 +3,10 @@ package org.example.bitcubetechnicalassignment;
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BitcubeTests extends BitcubeBase
 {
@@ -42,4 +43,43 @@ public class BitcubeTests extends BitcubeBase
         assertEquals(expectedNumber, number);
     }
 
+    @Test
+    void validateFactorialApiRequest()
+    {
+        String factorOfNumber = "5";
+        String expectedMethod = "POST";
+        String factorialUri = "/factorial";
+        AtomicReference<Request> apiRequest = new AtomicReference<>();
+
+        page.onRequest(request -> {
+
+            if (request.url().contains(factorialUri))
+            {
+                apiRequest.set(request);
+            }
+        });
+
+        Locator numberInput = page.locator("#number");
+        numberInput.fill(factorOfNumber);
+
+        Locator calculateButton = page.locator("#getFactorial");
+        calculateButton.click();
+        page.waitForTimeout(1000);
+
+        assertNotNull(apiRequest.get(), "Factorial API request was not made");
+
+        Request request = apiRequest.get();
+        String methodUsed = request.method();
+        assertEquals(expectedMethod, methodUsed);
+
+        assertTrue(request.postData().contains("number="+factorOfNumber), "API request body does not contain number="+factorOfNumber);
+
+        assertEquals("application/x-www-form-urlencoded; charset=UTF-8", request.headerValue("content-type"));
+        assertEquals("*/*", request.headerValue("Accept"));
+        assertEquals("keep-alive", request.headerValue("Connection"));
+        assertEquals("qainterview.pythonanywhere.com", request.headerValue("Host"));
+        assertEquals("https://qainterview.pythonanywhere.com", request.headerValue("Origin"));
+        assertEquals("XMLHttpRequest", request.headerValue("X-Requested-With"));
+        assertEquals("8", request.headerValue("Content-Length"));
+    }
 }
